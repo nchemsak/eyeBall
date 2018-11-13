bafa5bdc462efdf0c4219dc7769b510dc5f87da8
Nick <nchemsak@gmail.com>
2018-11-13 14:57:27 -0600

    back to top and anchor styling update

--- js/main.js
+++ js/main.js
@@ -77,10 +77,11 @@ $(document).ready(function() {
 
 
 // Back to top Button
-var amountScrolled = 200;
+var amountScrolled = 100;
 // var amountScrolledNav = 25;
 
 $(window).scroll(function() {
+
     if ($(window).scrollTop() > amountScrolled) {
         $('button.back-to-top').addClass('show');
     } else {
